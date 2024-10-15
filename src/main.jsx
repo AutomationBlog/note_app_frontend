8d69ba672fd6ad87040d84a21fb85eb9110ac363
automationblog <sawin24@gmail.com>
2024-10-15 18:09:30 +0530

    tailwind css config add

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import App from './App.jsx'
-import wip from "./assets/WIP.jpg";
+import App from "./App.jsx";
 import "./index.css";
+import Wip from "./Wip.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    <div id="portal">
-      <img src={wip} style={{ width: "100%", height: "100%" }} alt="WIP" />
-    </div>
+    <Wip />
   </StrictMode>
 );
