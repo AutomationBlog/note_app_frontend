2c8c1beacf9af0b728ab51d77bf1f77d0f1cb0fb
automationblog <sawin24@gmail.com>
2024-10-15 17:18:54 +0530

    main.jsx updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from './App.jsx'
+import wip from "./assets/WIP.jpg";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {/* <App /> */}
+    <div id="portal">
+      <img src={wip} style={{ width: "100%", height: "100%" }} alt="WIP" />
+    </div>
+  </StrictMode>
+);
